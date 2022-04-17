c440ad3c2c9f915372bdb942678d898d0d6ad94c
Bohdan Hrynkiv <grynkiv0x001@gmail.com>
2022-04-17 19:22:31 +0300

    Added Basic layout and content for every page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,26 @@
+import { Container, Heading, Text, Tab } from '../components/shared';
+
 const About = () => {
-  return <div className="about">About</div>;
+  return (
+    <div className="about">
+      <Container>
+        <Heading>Hello 👋</Heading>
+        <Text>
+          <Tab />
+          My name is Bohdan and this is first public version of my portfolio |
+          blog. Unfortunately, if you reading this text, the website is still in
+          development. But the good news are that the planned release date is
+          <strong> September 1st, 2022.</strong>
+          <hr />
+          <Tab />
+          Інформація для українських читачів - українська мова на сайті буде, я
+          додам вибір мови і також старатимусь писати статті одразу двома мовами
+          🇺🇦 🇬🇧. Проте з&apos;явиться така функція лише тоді, як я додам перший
+          реальний контент.
+        </Text>
+      </Container>
+    </div>
+  );
 };
 
 export default About;
