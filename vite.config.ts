a4c9d44c9ef33ebddfced1847789000a585c1f08
Bohdan Hrynkiv <grynkiv0x001@gmail.com>
2025-11-14 12:03:40 -0600

    github actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  base: '/', // Root path for GitHub Pages (username.github.io)
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
